d451a510b2d6a2f2c1763b5574e4cf52f3874091
Nicholas Tuck <nick@nicholastuck.com>
2014-06-14 01:38:17 -0500

    Fix car id global count bug

--- lib/car/CarStorage.dart
+++ lib/car/CarStorage.dart
@@ -9,11 +9,11 @@ class CarStorage {
   ObservableList<Car> cars = new ObservableList();
 
   CarStorage() {
-    cars
-      ..add(new Car(3, "Nissan", "Altima", false, Car.ENGINE_TYPES[0], -100.0, 27.0))
-      ..add(new Car(3, "Ford", "Focus", true, Car.ENGINE_TYPES[2], -98.2, 40.0))
-      ..add(new Car(3, "GMC", "4x4", false, Car.ENGINE_TYPES[1], -120.0, 37.0))
-      ..add(new Car(3, "Mazda", "3", true, Car.ENGINE_TYPES[3], 25.0, 27.0))
+    this
+      ..addCar(new Car(3, "Nissan", "Altima", false, Car.ENGINE_TYPES[0], -100.0, 27.0))
+      ..addCar(new Car(3, "Ford", "Focus", true, Car.ENGINE_TYPES[2], -98.2, 40.0))
+      ..addCar(new Car(3, "GMC", "4x4", false, Car.ENGINE_TYPES[1], -120.0, 37.0))
+      ..addCar(new Car(3, "Mazda", "3", true, Car.ENGINE_TYPES[3], 25.0, 27.0))
     ;
   }
 
