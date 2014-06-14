cfa6c64f10a184d1ff7a98e2040af9b78c38bb2e
Nicholas Tuck <nick@nicholastuck.com>
2014-06-14 18:56:10 -0500

    Get cesium map displaying on screen

--- lib/map/MapPanel.dart
+++ lib/map/MapPanel.dart
@@ -6,19 +6,48 @@ import 'package:dart_garage/car/Car.dart';
 import 'package:observe/observe.dart';
 import 'package:dart_garage/WatchingUtil.dart';
 import 'package:dart_garage/car/CarStorage.dart';
+import 'dart:js';
+import 'dart:html';
 
 @Component(
     selector: 'map-panel',
     templateUrl: 'packages/dart_garage/map/MapPanel.html',
     useShadowDom: false,
+
     publishAs: 'cmp'
 )
-class MapPanel implements AttachAware {
+class MapPanel extends ShadowRootAware implements AttachAware {
   Scope _scope;
+  CarStorage _carStorage;
+  List<Car> cars;
 
-  MapPanel(this._scope);
+  MapPanel(this._scope, this._carStorage);
 
   void attach() {
+    List<Car> cars = _carStorage.cars;
+
+//    createMap();
+  }
+
+
+  void onShadowRoot(ShadowRoot shadowRoot) {
+
+    createMap();
+  }
+
+  void createMap(){
+    var Cesium = context['Cesium'];
+//    var viewer = new JsObject(Cesium['Viewer'], ['cesiumMap']);
+    var widget = new JsObject(Cesium['CesiumWidget'], ['cesiumMap']);
+
+
+
+    // js interop example
+//    var object = new JsObject(context['Object']);
+//    object['greeting'] = 'Hello';
+//    object['greet'] = (name) => "${object['greeting']} $name";
+//    var message = object.callMethod('greet', ['JavaScript']);
+//    context['console'].callMethod('log', [message]);
 
   }
 
