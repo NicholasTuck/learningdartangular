8619c5de17f4f6d9185b7c01fc631534ccc3a1ed
Nicholas Tuck <nick@nicholastuck.com>
2014-06-14 19:37:32 -0500

    Got tanks added to the map using jsinterop manually

--- lib/map/MapPanel.dart
+++ lib/map/MapPanel.dart
@@ -9,45 +9,67 @@ import 'package:dart_garage/car/CarStorage.dart';
 import 'dart:js';
 import 'dart:html';
 
-@Component(
-    selector: 'map-panel',
-    templateUrl: 'packages/dart_garage/map/MapPanel.html',
-    useShadowDom: false,
+@Component(selector: 'map-panel', templateUrl: 'packages/dart_garage/map/MapPanel.html', useShadowDom: false,
 
-    publishAs: 'cmp'
-)
+publishAs: 'cmp')
 class MapPanel extends ShadowRootAware implements AttachAware {
   Scope _scope;
   CarStorage _carStorage;
   List<Car> cars;
 
+  var Cesium;
+  var scene;
+  var billboards;
+
   MapPanel(this._scope, this._carStorage);
 
   void attach() {
-    List<Car> cars = _carStorage.cars;
-
-//    createMap();
+    cars = _carStorage.cars;
   }
 
 
   void onShadowRoot(ShadowRoot shadowRoot) {
 
     createMap();
+    createBillboards();
+
   }
 
-  void createMap(){
-    var Cesium = context['Cesium'];
+  void createMap() {
+    Cesium = context['Cesium'];
 //    var viewer = new JsObject(Cesium['Viewer'], ['cesiumMap']);
-    var widget = new JsObject(Cesium['CesiumWidget'], ['cesiumMap']);
+    JsObject widget = new JsObject(Cesium['CesiumWidget'], ['cesiumMap']);
 
+    scene = widget['scene'];
 
+  }
 
-    // js interop example
-//    var object = new JsObject(context['Object']);
-//    object['greeting'] = 'Hello';
-//    object['greet'] = (name) => "${object['greeting']} $name";
-//    var message = object.callMethod('greet', ['JavaScript']);
-//    context['console'].callMethod('log', [message]);
+  void createBillboards() {
+    billboards = new JsObject(Cesium['BillboardCollection']);
+
+    ImageElement image = new ImageElement(src: "images/tank.png");
+    image.onLoad.listen((e) {
+      ImageElement image = e.target;
+      JsObject textureAtlas = new JsObject(Cesium['TextureAtlas'], [new JsObject.jsify({
+          'scene': scene, 'image': image
+      })]);
+
+      billboards['textureAtlas'] = textureAtlas;
+
+      for (Car car in cars) {
+        billboards.callMethod('add', [createBillboard(car)]);
+      }
+
+      scene['primitives'].callMethod('add', [billboards]);
+
+    });
+
+  }
+
+  JsObject createBillboard(Car car) {
+    return new JsObject.jsify({
+        'position': Cesium['Cartesian3'].callMethod('fromDegrees', [car.lon, car.lat]), 'imageIndex': 0, 'id': car.id
+    });
 
   }
 
